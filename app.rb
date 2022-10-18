70d92d805aa14a5106dff51b044a700eedf86de1
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-10-18 09:25:11 +0300

    completed assignment

---
+++ app.rb
@@ -0,0 +1,15 @@
+# app.rb
+puts "Hello world!"
+# app.rb
+puts "Hello world!"
+puts "Hello world!"
+puts "Hello world!"
+
+# app.rb
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2 ,3]
